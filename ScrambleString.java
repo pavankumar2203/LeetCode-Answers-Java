55b9c78ac655b3a19898a43de32e8bac8d1e1d6b
Yadong Wen <ralph.wen@gmail.com>
2014-05-27 10:05:23 -0500

    Create ScrambleString.java

---
+++ ScrambleString.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+    public boolean isScramble(String s1, String s2) {
+        if(s1.equals(s2)) return true;
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        if(!Arrays.equals(c1, c2)) return false;
+        
+        for(int i = 1; i < s1.length(); i++) {
+            String s11 = s1.substring(0, i);
+            String s12 = s1.substring(i);
+            String s21 = s2.substring(0, i);
+            String s22 = s2.substring(i);
+            String s212 = s2.substring(0, s2.length()-i);
+            String s222 = s2.substring(s2.length()-i);
+            if(isScramble(s11, s21) && isScramble(s12, s22)) return true;
+            if(isScramble(s11, s222) && isScramble(s12, s212)) return true;
+        }
+        return false;
+    }
+}
